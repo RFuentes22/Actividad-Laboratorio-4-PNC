8a8656bbbd80e718aa0f61aa9a72ab4def113b2f
RFuentes <00062217@uca.edu.sv>
2020-05-06 14:38:37 -0600

    Initial commit

---
+++ src/main/java/com/uca/capas/Laboratorio4NCapasApplication.java
@@ -0,0 +1,13 @@
+package com.uca.capas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboratorio4NCapasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Laboratorio4NCapasApplication.class, args);
+	}
+
+}
